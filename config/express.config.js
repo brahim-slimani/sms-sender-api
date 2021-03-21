cfc7dfb07233e814197eb4cb3e958443472b14e3
Slimani-Ibrahim <slimani.ibr@gmail.com>
2021-03-21 09:45:14 +0100

    allow cors OPTIONS for http requests

--- config/express.config.js
+++ config/express.config.js
@@ -8,6 +8,9 @@ const morgan = require("morgan");
 //DATA BINDING INTO JSON FROM HTTP REQUEST
 const bodyParser = require("body-parser");
 
+//CORS ORIGIN FOR HTTP REQUESTS
+const cors = require("cors");
+
 //ENABLE .env CONFIG
 const dotenv = require("dotenv");
 dotenv.config();
@@ -18,6 +21,9 @@ const routes = require("../routes/router");
 config.use(morgan("combined"));
 config.use(bodyParser.json());
 config.use("/", routes);
+
+//allow OPTIONS on all resources
+config.options('*', cors());
 
 //EXPORT CONFIG
 module.exports = config;
